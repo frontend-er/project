918a821de6b6e16fe7efc4c363a2f7581986daaf
Dimas <dbjenov@gmail.com>
2021-04-21 12:11:36 +0300

    filme console

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-let number = 5;
+//Создаем переменную и помещаем в нее ответы на вопросы пользователя
+
+const numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?", "");
+
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   geners: {},
+   privat: false,
+
+
+};
+
+
+const lastFilm = prompt("Один из просмотренных фильмов?", "");
+const lastFilmRating = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[lastFilm] = lastFilmRating;
+
+
+
+console.log(personalMovieDB);
+
+
+console.log(lastFilm);
